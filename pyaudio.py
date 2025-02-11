07d91e0784441bc72699cbc785dc05ccbac5d5fe
sakamod <sakamod36@gmail.com>
2025-02-11 15:46:25 +0900

    firestcommit

---
+++ pyaudio.py
@@ -0,0 +1,15 @@
+import pyaudio
+
+def list_audio_devices():
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    num_devices = info.get('deviceCount')
+    
+    for i in range(0, num_devices):
+        device_info = p.get_device_info_by_host_api_device_index(0, i)
+        if device_info.get('maxOutputChannels') > 0:
+            print(f"Device ID: {i}, Name: {device_info.get('name')}")
+    
+    p.terminate()
+
+list_audio_devices()
